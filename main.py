2494a1c066b83033d45bc84dd1d10d190125fd70
DaianaSaenz <97228389+DaianaSaenz@users.noreply.github.com>
2023-02-19 22:32:35 +0100

    añadiendo metodos a la clase

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def juego():
 
     tablero_jugador= clases.Tablero()
     tablero_jugador.posicionar_barcos()
+    print(tablero_jugador.mostrar_tablero())
 
     # 3. Turnos disparar, ALTERNAN TURNOS si disparo erroneo y comprobar si todos los barcos esyan hundidos
     # 5. Si hay ganador condicion while false entonces fin juego
